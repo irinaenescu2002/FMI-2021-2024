e1f31d707ef8ff4bb6ebfa99767c650f1430b02c
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2022-11-25 23:33:17 +0200

    Create helloworld.asm

---
+++ Anul I - Semestrul I/Arhitectura Sistemelor de Calcul (ASC)/Laborator/Programe Laborator/helloworld.asm
@@ -0,0 +1,15 @@
+.data
+	helloworld: .asciz "Hello, world!\n"
+.text
+.globl _start
+
+_start:
+	mov $4, %eax
+	mov $1, %ebx
+	mov $helloworld, %ecx
+	mov $15, %edx
+	int $0x80
+
+	mov $1, %eax
+	mov $0, %ebx
+	int $0x80
